da1a8186a4061cbf472c98c484c9d8dd2db07106
andresmafla <65826237+andresmafla@users.noreply.github.com>
2022-12-03 13:58:44 -0500

    primer mejora

--- conecta.php
+++ conecta.php
@@ -1,14 +1,11 @@
 <?php
-$servername = "localhost";
-$database = "usuarios";
-$username = "";
-$password = "";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
+    function conectar(){
+        $bd = mysqli_connect("localhost","root","", "agenda_contactos");
+        if (!$bd) {
+            echo "<h3>Error de conexion</h3>";
+            return NULL;
+        }else{
+            return $bd;
+        }
+    }
 ?>
---
+++ menu.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            <img src="imagenes/logo.png" class="img-thumbnail" alt="...">
+        </a>
+        <h1>Universidad de Nariño</h1>
+    </div>
+</nav>
